bd99691ec0ed43194ca7a4120a966a521e980212
Ethan McMahan <cowboyfanet@hotmail.com>
2022-11-07 11:56:31 -0800

    edited css for index page.

--- recipes.js
+++ recipes.js
@@ -1,9 +1,4 @@
-let foodChoice = Math.floor(Math.random());
-function clickButton(foodChoice) {
-    console.log(foodChoice);
-};
-
-const findRecipesButton = document.querySelector("#findRecipes");
+const findRecipesButton = document.querySelector("#findRecipes")
 findRecipesButton.addEventListener("click", function(){
-    console.log("#findRecipes", click);
+    console.log("#findRecipes", foodChoice);
 });
